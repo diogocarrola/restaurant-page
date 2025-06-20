ab7cb4c0d9040c189627c4d17f16281bf2a845f9
Diogo Carrola <diogo.carrola@gmail.com>
2025-06-20 11:57:02 +0100

    Add CSS and style loaders to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,12 @@ module.exports = {
   devServer: {
     static: './dist',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
